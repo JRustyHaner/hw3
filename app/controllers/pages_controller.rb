a18e4f787c1207cbb61a568db142441461e78d69
JRustyHaner <jrhaner@memphis.edu>
2019-01-29 22:54:56 +0000

    Step 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,8 @@
 class PagesController < ApplicationController
+    def jrhaner
+        render 'jrhaner.html.erb'
+    end
+    def review
+        render 'review.html.erb'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#jrhaner'
+  get 'jrhaner', to 'pages#jrhaner', as "jrhaner"
+  get 'review', to 'pages#review', as "review"
 end
