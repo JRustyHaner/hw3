44ef7d010e52b6fd0cbfd089b41c5d4c956cd17c
JRustyHaner <jrhaner@memphis.edu>
2019-02-04 18:58:57 +0000

    Route fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,5 @@
 class PagesController < ApplicationController
-    def jrhaner
-        render 'jrhaner.html.erb'
-    end
-    def review
-        render 'review.html.erb'
-    end
+    def show
+        render template: "pages/#{params[:page]}"
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#jrhaner'
-  get 'jrhaner', to 'pages#jrhaner', as "jrhaner"
-  get 'review', to 'pages#review', as "review"
+  get "/pages/:page" => "pages#show"
 end
